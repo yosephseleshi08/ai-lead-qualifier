b0e4cb964e17cb20f560706a13f7bad1116471b9
yosephseleshi08 <yosephseleshi08@gmail.com>
2026-08-30 22:13:10 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["localhost", "images.unsplash.com", "ui-avatars.com"],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
