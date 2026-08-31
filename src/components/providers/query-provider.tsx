9afaaa34270c93e6b4bd3e44130bd41d74e30b1a
yosephseleshi08 <yosephseleshi08@gmail.com>
2026-08-31 13:05:43 +0300

    Update query-provider.tsx

--- src/components/providers/query-provider.tsx
+++ src/components/providers/query-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
@@ -21,7 +20,6 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
